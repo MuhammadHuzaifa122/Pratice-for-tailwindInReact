c2f7f14e0ec495b1cc286f3319b57ca6d8f3954d
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-10-03 14:59:43 +0500

    Created list items in header and will provide them routing later

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import viteLogo from '/vite.svg'
+import React from "react";
+import viteLogo from "/vite.svg";
 
 export default function Header() {
   return (
-    <header className='flex justify-between py-4 px-8 shadow-md'>
-        <img src={viteLogo} alt="viteLogo" />
-        <ul className='flex gap-4 '>
-            <li>
-                <a href="/">Home</a>
-            </li>
-            <li>
-                <a href="about">About</a>
-            </li>
-            <li>
-                <a href="contact">Contact</a>
-            </li>
-        </ul>
+    <header className="flex justify-between px-4 md:px-8 shadow-md py-4">
+      <img src={viteLogo} alt="viteLogo" />
+      <ul className="flex gap-4 ">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="about">About</a>
+        </li>
+        <li>
+          <a href="contact">Contact</a>
+        </li>
+      </ul>
     </header>
-  )
+  );
 }
