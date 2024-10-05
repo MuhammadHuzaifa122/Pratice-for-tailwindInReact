2333e41db68ee436d186655e595312e8f4c97de0
Muhammad Huzaifa Nadeem <f2020266384@umt.edu.pk>
2024-10-05 14:29:13 +0500

    Convert About component to lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import { Suspense } from "react";
 
 function App() {
   return (
     <>
       <Header />
       <main className="py-4 px-4 md:px-8">
-        <Outlet />
+        <Suspense fallback={<div className="text-xl">Loading...</div>}>
+          <Outlet />
+        </Suspense>
       </main>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
 import Home from "./components/Home.jsx";
 import { createRoot } from "react-dom/client";
+import { lazy } from "react";
+
+const About = lazy(() =>
+  wait(1000).then(() => import("./components/About.jsx"))
+);
 
 const router = createBrowserRouter([
   {
@@ -27,6 +31,14 @@ const router = createBrowserRouter([
     ],
   },
 ]);
+
+const wait = (time) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, time);
+  });
+};
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
